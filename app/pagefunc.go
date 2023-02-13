4a05eada5a40e5c39dc3f581a6140fbfb7ffd175
wangjuntao <jamiesun.net@gmail.com>
2023-02-13 12:56:48 +0800

    : pagever

--- app/pagefunc.go
+++ app/pagefunc.go
@@ -10,7 +10,11 @@ import (
 func (a *Application) GetTemplateFuncMap() map[string]interface{} {
 	return map[string]interface{}{
 		"pagever": func() int64 {
-			return time.Now().Unix()
+			if a.appConfig.System.Debug {
+				return time.Now().Unix()
+			} else {
+				return int64(time.Now().Hour())
+			}
 		},
 		"buildver": func() string {
 			bv := strings.TrimSpace(assets.BuildVersion())
